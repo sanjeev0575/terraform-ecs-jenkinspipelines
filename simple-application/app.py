b6ffacb6bca5e01314eb1445a6559c287d38656d
Aditya <sanjeev0575@gmail.com>
2025-08-28 16:28:52 +0530

    add the dockerfile in port number

--- simple-application/app.py
+++ simple-application/app.py
@@ -1,6 +1,20 @@
-from flask import Flask
+# from flask import Flask
+# app = Flask(__name__)
+
+# @app.route('/')
+# def hello():
+#     return 'Hello, World!'
+
+
+from flask import Flask, request, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def hello():
-    return 'Hello, World!'
+    name = request.args.get("name", "World")
+    return jsonify(message=f"Hello, {name}!")
+
+if __name__ == "__main__":
+    # simple dev server (not for production)
+    app.run(host="0.0.0.0", port=9000)
